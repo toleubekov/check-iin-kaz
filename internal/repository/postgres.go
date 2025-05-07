1dafcde2fc695c5b377313f92ed45bd5999a7e91
Zhandos <zhandoslp@gmail.com>
2025-05-07 14:33:20 +0500

    dummy postgres

---
+++ internal/repository/postgres.go
@@ -0,0 +1,33 @@
+package repository
+
+import (
+	"github.com/jmoiron/sqlx"
+	"github.com/toleubekov/kaspiCheckIIN/internal/model"
+)
+
+type PersonRepository struct {
+	db *sqlx.DB
+}
+
+func NewPersonRepository(db *sqlx.DB) *PersonRepository {
+	return &PersonRepository{db: db}
+}
+
+func (r *PersonRepository) Create(person *model.Person) error {
+	return nil
+}
+
+func (r *PersonRepository) GetByIIN(iin string) (*model.Person, error) {
+	var person model.Person
+	return &person, nil
+}
+
+func (r *PersonRepository) FindByNamePart(namePart string) ([]model.Person, error) {
+	var people []model.Person
+	return people, nil
+}
+
+func InitDB(connectionString string) (*sqlx.DB, error) {
+	db := &sqlx.DB{}
+	return db, nil
+}
