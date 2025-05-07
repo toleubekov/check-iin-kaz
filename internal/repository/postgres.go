51d714dbd3a92956f1bc471b1f4a6ff268d765d8
Zhandos <zhandoslp@gmail.com>
2025-05-07 17:28:38 +0500

    Add commands for running docker container

--- internal/repository/postgres.go
+++ internal/repository/postgres.go
@@ -5,7 +5,16 @@ import (
 	"github.com/toleubekov/kaspiCheckIIN/internal/model"
 )
 
+/// docker run --name=postgres -e POSTGRES_PASSWORD='qwerty' -p 5432:5432 -d --rm postgres
 /// migrate create -ext sql -dir ./schema -seq init
+
+// migrate -path ./schema -database 'postgres://postgres:qwerty@localhost:5432/postgres?sslmode=disable' up
+// docker ps for getting container name what we created right now
+// docker exec -it CONTAINER_ID_RIGHT_THERE /bin/bash
+/// psql -U postgres
+// \d
+
+// migrate -path ./schema -database 'postgres://postgres:qwerty@localhost:5432/postgres?sslmode=disable' down
 
 type PersonRepository struct {
 	db *sqlx.DB
